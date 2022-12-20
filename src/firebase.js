9b023978f8fad5c61eea553e433016117cd7ea5b
simoneya <yavar.simona@yahoo.com>
2022-12-20 15:47:18 +0000

    starting files

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCXoiepgNmYm5w6lVmPHMN9UUSrgxQpwd4",
+    authDomain: "whats-app-clone-41618.firebaseapp.com",
+    projectId: "whats-app-clone-41618",
+    storageBucket: "whats-app-clone-41618.appspot.com",
+    messagingSenderId: "1061088344436",
+    appId: "1:1061088344436:web:73556db35cd42766bd1421",
+    measurementId: "G-YPQFRHEJB2"
+  };
